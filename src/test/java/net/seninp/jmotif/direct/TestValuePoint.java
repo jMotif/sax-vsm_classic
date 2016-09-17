83be64c9ff4e69dafc5aa4df1212e1c4c23237b0
Pavel Senin <seninp@gmail.com>
2016-09-17 15:31:48 -0600

    adding tests

--- src/test/java/net/seninp/jmotif/direct/TestValuePoint.java
+++ src/test/java/net/seninp/jmotif/direct/TestValuePoint.java
@@ -4,12 +4,11 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
-import java.util.Arrays;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 
-public class TestPoint {
+public class TestValuePoint {
 
   @BeforeClass
   public static void setUpBeforeClass() throws Exception {
@@ -21,29 +20,25 @@ public class TestPoint {
 
   @Test
   public void testPoint() {
-    Point p1 = Point.at(10.0);
-    Point p2 = Point.at(10.0);
+    ValuePoint p1 = ValuePoint.at(Point.at(10.0), 1.0);
+    ValuePoint p2 = ValuePoint.at(Point.at(10.0), 1.0);
 
-    Point p3 = Point.at(11.0);
+    ValuePoint p3 = ValuePoint.at(Point.at(13.0), 1.0);
+    ValuePoint p4 = ValuePoint.at(Point.at(10.0), 2.0);
 
     assertTrue(p1.equals(p2));
     assertEquals(p1.hashCode(), p2.hashCode());
 
-    assertFalse(p3.equals(p2));
+    assertFalse(p1.equals(p3));
     assertNotEquals(p3.hashCode(), p2.hashCode());
 
-    assertTrue(p1.toString().contains("10.0"));
-    assertTrue(p1.toString().contains("]"));
+    assertFalse(p1.equals(p4));
+    assertNotEquals(p4.hashCode(), p2.hashCode());
 
-    assertTrue(p1.toLogString().contains("10.0"));
-    assertFalse(p1.toLogString().contains("]"));
+    assertTrue(p1.toString().contains("1.0@"));
 
-    assertTrue(Arrays.equals(Point.at(10.49, 10.51).toIntArray(), new int[] { 10, 11 }));
-
-    assertTrue(Point.getDefault().equals(Point.at()));
-
-    Point p4 = Point.random(11);
-    assertEquals(11, p4.toArray().length);
+    assertEquals(1, p1.getValue(), 0.000001);
+    assertEquals(Point.at(10.0), p1.getPoint());
 
   }
 
